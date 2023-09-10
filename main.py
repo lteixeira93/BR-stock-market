64d407e42ce8b80e5e69866d9d53645e81ffff62
Luiz Teixeira <lteixeira93@outlook.com>
2023-09-10 12:08:31 +0100

    Add get_stocks_table and filter_stocks_table method on financial tables.

--- main.py
+++ main.py
@@ -1,31 +1,64 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver import chrome, Keys
+import pandas as pd
+import requests
 
 
-class stock_web_driver():
-    def __init__(self):
-        pass
+class StockWebDriver:
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(StockWebDriver, cls).__new__(cls)
+        return cls.instance
 
-    def driver_parser(self):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("detach", True)
+    def __init__(self) -> None:
+        self.__stock_data_frame = None
+        self.__drop_columns_list = [
+            'Empresa', 'Data Preço', 'Data Dem.Financ.', 'Consolidação', 'ROTanC', 'ROInvC', 'RPL',	'ROA', 'Margem Líquida', 'Margem Bruta',
+            'Giro Ativo', 'Alav.Financ.', 'Passivo/PL', 'Preço/Lucro', 'Preço/VPA', 'Preço/Rec.Líq.', 'Preço/FCO' ,'Preço/FCF', 'Preço/EBIT',
+            'Preço/NCAV', 'Preço/Ativo Total', 'Preço/Cap.Giro', 'EV/EBITDA', 'EV/Rec.Líq.', 'EV/FCF', 'EV/FCO', 'EV/Ativo Total',
+            'Market Cap(R$)', '# Ações Total', '# Ações Ord.', '# Ações Pref.'
+        ]
 
-        driver = webdriver.Chrome(options=options)
+        self.__url: str = 'https://shorturl.at/hrGHQ'
+        self.__user_agent: str = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                  'Chrome/50.0.2661.75 Safari/537.36')
+        self.__url_request_type: str = 'XMLHttpRequest'
+        self.__header = {
+          "User-Agent": self.__user_agent,
+          "X-Requested-With": self.__url_request_type
+        }
+        self.__request = requests.get(self.__url, headers=self.__header)
 
-        with driver:
-            driver.get("https://shorturl.at/hrGHQ")
+    def get_stocks_table(self) -> None:
+        r"""
+        Get HTML stock tables into a list of DataFrame objects.
 
-            # time.sleep(5)
+        Returns
+        -------
+        A list of DataFrames.
+        """
+        self.__stock_data_frame = pd.concat(pd.read_html(self.__request.text))
+        print(self.__stock_data_frame)
+        self.filter_stocks_table()
 
-            print(driver.title)
-            download_button = driver.find_element(By.ID, "botao_arquivo")
-            # download_button.click()
-            download_button.send_keys(Keys.ENTER)
-            print("Button Clicked")
+    def filter_stocks_table(self) -> None:
+        r"""
+        Filter stock tables list of DataFrame objects based on:
+        EBIT Margin (%)
+        EV/EBIT
+        Dividend Yield (%)
+        Financial Volume (%)
+
+        Returns
+        -------
+        A list of DataFrames filtered.
+        """
+        self.__stock_data_frame.drop(columns=self.__drop_columns_list, inplace=True)
+        self.__stock_data_frame = self.__stock_data_frame.drop(self.__stock_data_frame['Volume Financ.(R$)'] < '1_000_000') # TODO: Remove Financial volumes
+        print(self.__stock_data_frame)
+
 
 def main():
-    stock_web_driver.driver_parser()
+    stock_web_driver = StockWebDriver()
+    stock_web_driver.get_stocks_table()
 
 
 if __name__ == "__main__":
