c785888a15ec6b7c1dcc08f0d0a713127c3ca5d1
Luiz Teixeira <lteixeira93@outlook.com>
2023-11-08 12:14:06 +0000

    Create helper method to verify if a text is present in a given xpath.

--- utils/helper.py
+++ utils/helper.py
@@ -1,6 +1,20 @@
 import pandas as pd
+from selenium.webdriver.remote.webelement import WebElement
 
 
 def print_full_dataframe(dataframe) -> None:
+    r"""
+    Print all dataframe fields.
+    """
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(dataframe)
+
+
+def is_text_in_xpath(text: str, xpath: WebElement) -> bool:
+    r"""
+    Find if desired text is present in the provided XPATH.
+    """
+    if text in xpath.text:
+        return True
+    else:
+        return False
